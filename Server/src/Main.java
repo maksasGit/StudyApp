8873d0b49a57e821076b5e15245fb6aab1b3a79d
maksasGit <maksim.rishko.vit@gmail.com>
2023-05-19 18:17:08 +0200

    DataBase reform

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(5000);
+        Server server = new Server(5001);
         server.listen();
     }
 }
--- Server/src/Storage.java
+++ Server/src/Storage.java
@@ -1,13 +1,35 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Storage {
     private List<Test> tests;
 
     public Storage() {
+        connect();
         this.tests = new ArrayList<>(Arrays.asList(new Test("Tect 1"), new Test("Test2"), new Test("Test nomer 3"), new Test("Examination Test №4")));
     }
+
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            // Load the SQLite JDBC driver
+            Class.forName("org.sqlite.JDBC");
+
+            // Establish a connection to the database
+            String url = "jdbc:sqlite:C:/Users/imaks/OneDrive/Desktop/StudyApp/Server/DataBase/DB.sqlite";
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connected to the database.");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Failed to connect to the database: " + e.getMessage());
+        }
+        return connection;
+    }
+
+
 
     public List<Test> getTests() {
         return tests;
