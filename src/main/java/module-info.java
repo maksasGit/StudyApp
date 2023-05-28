e9aafab4bc627d7f1e7baffef8de229d9df321a2
maksasGit <maksim.rishko.vit@gmail.com>
2023-05-28 16:19:25 +0200

    Delete main

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                        requires org.kordamp.bootstrapfx.core;
-            
-    opens main.demo to javafx.fxml;
-    exports main.demo;
-}
